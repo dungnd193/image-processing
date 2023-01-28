2ca1c438c4f77d16155d14fe634f4d0b4b5df5f0
dungnd193 <dungnd.work@gmail.com>
2023-01-28 20:48:21 +0700

    check name equal main

--- gui.py
+++ gui.py
@@ -57,28 +57,29 @@ def hybrid_filter():
     except:
         print("An exception occurred")
 
-window = tk.Tk()
-window.title("Image Processing")
+if __name__ == "__main__":
+    window = tk.Tk()
+    window.title("Image Processing")
 
-window.rowconfigure(0, minsize=350, weight=1)
-window.columnconfigure(1, minsize=350, weight=1)
+    window.rowconfigure(0, minsize=350, weight=1)
+    window.columnconfigure(1, minsize=350, weight=1)
 
-lbl = tk.Label(window)
-frm_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
+    lbl = tk.Label(window)
+    frm_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
 
-btn_open = tk.Button(frm_buttons, text="Open", command=open_image)
-btn_hpf = tk.Button(frm_buttons, text="High Pass Filter", command=hpf)
-btn_lpf = tk.Button(frm_buttons, text="Low Pass Filter", command=lpf)
-btn_gf = tk.Button(frm_buttons, text="Gradient Filter", command=gradient_filter)
-btn_hf = tk.Button(frm_buttons, text="Hybrid Filter", command=hybrid_filter)
+    btn_open = tk.Button(frm_buttons, text="Open", command=open_image)
+    btn_hpf = tk.Button(frm_buttons, text="High Pass Filter", command=hpf)
+    btn_lpf = tk.Button(frm_buttons, text="Low Pass Filter", command=lpf)
+    btn_gf = tk.Button(frm_buttons, text="Gradient Filter", command=gradient_filter)
+    btn_hf = tk.Button(frm_buttons, text="Hybrid Filter", command=hybrid_filter)
 
-btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
-btn_hpf.grid(row=1, column=0, sticky="ew", padx=5, pady=5)
-btn_lpf.grid(row=2, column=0, sticky="ew", padx=5, pady=5)
-btn_gf.grid(row=3, column=0, sticky="ew", padx=5, pady=5)
-btn_hf.grid(row=4, column=0, sticky="ew", padx=5, pady=5)
+    btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+    btn_hpf.grid(row=1, column=0, sticky="ew", padx=5, pady=5)
+    btn_lpf.grid(row=2, column=0, sticky="ew", padx=5, pady=5)
+    btn_gf.grid(row=3, column=0, sticky="ew", padx=5, pady=5)
+    btn_hf.grid(row=4, column=0, sticky="ew", padx=5, pady=5)
 
-frm_buttons.grid(row=0, column=0, sticky="ns")
-lbl.grid(row=0, column=1, sticky="nsew")
+    frm_buttons.grid(row=0, column=0, sticky="ns")
+    lbl.grid(row=0, column=1, sticky="nsew")
 
-window.mainloop()
+    window.mainloop()
